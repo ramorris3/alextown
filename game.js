fbe3dca0d8e6844c157e86f12509c0c24c56e934
Alex Morris <r.alex.morris.3@gmail.com>
2016-02-22 00:45:27 -0700

    fixed refactor to include rooks (rooks need help with arrow logic)

--- game.js
+++ game.js
@@ -55,7 +55,7 @@ GameState.prototype.create = function() {
             (this.game.height / 2) + (this.PLAYER_SPRITE_HEIGHT / 2))
     );
 
-    // create chomper sprite
+    // create chomper sprites
     this.chomper_swarm = this.game.add.group();
     for (var y = 0; y < this.game.height; y += this.CHOMPER_SPRITE_HEIGHT) {
         var chomper = this.game.add.existing(
@@ -64,12 +64,30 @@ GameState.prototype.create = function() {
         this.chomper_swarm.add(chomper);
     }
 
+    // create charger sprites
     this.charger_swarm = this.game.add.group();
     for (var y = 0; y < this.game.height; y += this.CHARGER_SPRITE_HEIGHT) {
         var charger = this.game.add.existing(
             new Charger(this.game, this.game.width, y)
             );
-        this.charger_swarm.add(charger)
+        this.charger_swarm.add(charger);
+    }
+
+    // create arrow pool for rooks
+    this.arrowpool = this.game.add.group();
+    for (var i = 0; i<100; i++) {
+        var arrow = this.game.add.existing(
+            new Arrow(this.game, 0, 0)
+        );
+        this.arrowpool.add(arrow);
+    }
+
+    // create rooks
+    this.rook_troop = this.game.add.group();
+    for (var z = 0; z < this.game.height; z += this.ROOK_SPRITE_HEIGHT) {
+        var rook = this.game.add.existing(
+            new Rook(this.game, this.game.width - this.ROOK_SPRITE_WIDTH, z, this.player, this.arrowpool)
+        );
     }
 
 	// create ground

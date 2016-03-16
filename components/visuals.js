0f8368ef87d6f58e7f12563d2495ecd026aa98dd
ramorris3 <r.alex.morris.3@gmail.com>
2016-03-16 12:13:34 -0600

    got slice to work, need to fix animation and alpha

--- components/visuals.js
+++ components/visuals.js
@@ -8,6 +8,7 @@ var Slices = function(game) {
 		slice.animations.add('slice1', [0,1,2,3,4,5,6,7,8], 30, false);
 		slice.animations.add('slice2', [9,10,11,12,13,14,15], 30, false);
 		slice.animations.add('slice3', [16,17,18,19,20,21,22,23], 30, false);
+		slice.anchor.setTo(0.5,0.5);
 		slice.kill();
 		this.slices.add(slice);
 	}
@@ -19,18 +20,16 @@ Slices.prototype.constructor = Slices;
 Slices.prototype.slice = function(x, y) {
 	// get a slice sprite, and choose a random slice animation to play
 	var slice = this.slices.getFirstDead();
-	var animKey = this.animationKeys[Math.floor(Math.random() * 3) - 1];
-	console.log(animKey);
+	var animKey = this.animationKeys[Math.floor(Math.random() * 3)];
 
-	// revive and play animation (will die when enimation ends)
+	// revive and play animation (will die when animation ends)
 	slice.reset(x, y);
 	slice.revive();
-	slice.killOnComplete = true;
-	slice.play(animKey);
+	slice.play(animKey, 40, false, true);
 
 	// fade as animation plays
 	slice.update = function() {
-		this.alpha -= .02;
+		this.alpha -= .05;
 
 	};
 };

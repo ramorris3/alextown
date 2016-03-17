bef4d472cb6872adfc5c13ed3c231578809150de
ramorris3 <r.alex.morris.3@gmail.com>
2016-03-17 12:08:55 -0600

    updated slice sprite

--- components/visuals.js
+++ components/visuals.js
@@ -1,13 +1,12 @@
 var Slices = function(game) {	
 	// init 100 slice sprites
 	this.slices = game.add.group();
-	this.animationKeys = ['slice1', 'slice2', 'slice3']; // for slice() later
+	this.animationKeys = ['slice1', 'slice2']; // for slice() later
 	for (var i = 0; i < 100; i++)
 	{
 		var slice = new Phaser.Sprite(game, 0, 0, 'slices');
-		slice.animations.add('slice1', [0,1,2,3,4,5,6,7,8], 30, false);
-		slice.animations.add('slice2', [9,10,11,12,13,14,15], 30, false);
-		slice.animations.add('slice3', [16,17,18,19,20,21,22,23], 30, false);
+		slice.animations.add('slice1', [0,1,2,3,4,5,6,7,8,9,10,11,12]);
+		slice.animations.add('slice2', [13,14,15,16,17,18,19,20,21,22,23,24,25]);
 		slice.anchor.setTo(0.5,0.5);
 		slice.kill();
 		this.slices.add(slice);
@@ -20,7 +19,7 @@ Slices.prototype.constructor = Slices;
 Slices.prototype.slice = function(self) {
 	// get a slice sprite, and choose a random slice animation to play
 	var slice = this.slices.getFirstDead();
-	var animKey = this.animationKeys[Math.floor(Math.random() * 3)];
+	var animKey = this.animationKeys[Math.floor(Math.random() * 2)];
 
 	// revive and play animation (will die when animation ends)
 	slice.reset(self.x, self.y);
